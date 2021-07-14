86ce6d496915219b2c47b5f39a8a2708da254c20
niche-24 <87414408+niche-24@users.noreply.github.com>
2021-07-14 14:48:01 +0700

    Update config.js

--- config.js
+++ config.js
@@ -1,19 +1,16 @@
 const CONFIG = {
-    introTitle: 'Tên người yêu bạn à!',
-    introDesc: `Trái đất vốn lạ thường
-    Mà sao em cứ đi nhầm đường
-    Lạc vào tim anh lẻ loi
-    Đằng sau chữ yêu đây là thương`,
-    btnIntro: 'hihi',
-    title: 'Phải chăng em đã yêu ngay từ cái nhìn đầu tiên 😙',
-    desc: 'Phải chăng em đã say ngay từ lúc thấy nụ cười ấy ',
-    btnYes: 'Vẫn cứ là thích anh <33',
-    btnNo: 'Không, Anh trai à :3',
-    question:'Trên thế giới hơn 7 tỉ người mà sao em lại yêu anh <3',
-    btnReply: 'Gửi cho anh <3',
-    reply: 'Yêu thì yêu mà không yêu thì yêu <33333333',
-    mess: 'Anh biết mà 🥰. Yêu em nhiều nhiều 😘😘',
-    messDesc: 'Tối nay 7h anh qua đón nhé công chúa.',
-    btnAccept: 'Okiiiii lun <3',
-    messLink: 'http://fb.com' //link mess của các bạn. VD: https://m.me/nam.nodemy
+    introTitle: 'Diệp Chi, right?',
+    introDesc: `You know what?`,
+    btnIntro: 'You look cute today',
+    title: 'I bet you have a good day. But,...',
+    desc: 'Will you text me first tonight?',
+    btnYes: 'Yessss <33',
+    btnNo: 'Why I have to? :3',
+    question:'Want to hear my voice?',
+    btnReply: 'I want to hear yours too <3',
+    reply: 'What about a call?',
+    mess: 'Or, maybe let me sing to you =))',
+    messDesc: 'Text me? tonight 9pm?',
+    btnAccept: 'Yeah <3',
+    messLink: 'http://https://www.facebook.com/anhthu.leho/' 
 }
